ecdb730751809f3596106e70281f162be8d7f4d6
Zim Gong <zimgong@umich.edu>
2024-10-24 19:46:06 -0400

    add custom ekf slam

---
+++ src/ekf_slam/CHANGELOG.rst
@@ -0,0 +1,8 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package ekf_slam
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+0.0.0 (2024-10-24)
+-------------------
+* Initialize reinzor
+* Contributors: Zim Gong
---
+++ src/ekf_slam/src/ekf_slam_app.cpp
@@ -0,0 +1,23 @@
+
+#include "ekf_slam/ekf_slam_wrapper.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "mrpt_ekf_slam_2d");
+  ros::NodeHandle n;
+  ros::Rate r(100);
+  EKFslamWrapper slam;
+  slam.get_param();
+  slam.init();
+  ros::Duration(3).sleep();
+
+  if (!slam.rawlogPlay())
+  {  // if not play from rawlog file
+
+    while (ros::ok())
+    {
+      ros::spinOnce();
+      r.sleep();
+    }
+  }
+}
